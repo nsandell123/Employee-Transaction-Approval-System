90bf872666a13660feb0bc208f32fa404ba52ef3
Neel Sandell <nsandell123@Neels-MacBook-Pro.local>
2024-08-24 12:40:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Employee Transaction Approval System
+
+To run, run `npm install` and then `yarn start`
+
---
+++ src/utils/requests.ts
@@ -0,0 +1,66 @@
+import {
+  PaginatedRequestParams,
+  PaginatedResponse,
+  RequestByEmployeeParams,
+  SetTransactionApprovalParams,
+  Transaction,
+  Employee,
+} from "./types"
+import mockData from "../mock-data.json"
+
+const TRANSACTIONS_PER_PAGE = 5
+
+let data: { employees: Employee[]; transactions: Transaction[] } = {
+  employees: mockData.employees,
+  transactions: mockData.transactions,
+}
+
+export const getEmployees = (): Employee[] => data.employees
+
+export const getTransactionsPaginated = ({
+  page,
+}: PaginatedRequestParams): PaginatedResponse<Transaction[]> => {
+  if (page === null) {
+    throw new Error("Page cannot be null")
+  }
+
+  const start = page * TRANSACTIONS_PER_PAGE
+  const end = start + TRANSACTIONS_PER_PAGE
+
+  if (start > data.transactions.length) {
+    throw new Error(`Invalid page ${page}`)
+  }
+
+  const nextPage = end < data.transactions.length ? page + 1 : null
+
+  return {
+    nextPage,
+    data: data.transactions.slice(start, end),
+  }
+}
+
+export const getTransactionsByEmployee = ({ employeeId }: RequestByEmployeeParams) => {
+  if (!employeeId) {
+    throw new Error("Employee id cannot be empty")
+  }
+
+  return data.transactions.filter((transaction) => transaction.employee.id === employeeId)
+}
+
+export const setTransactionApproval = ({ transactionId, value }: SetTransactionApprovalParams) => {
+  const transaction = data.transactions.find(
+    (currentTransaction) => currentTransaction.id === transactionId
+  )
+
+  if (!transaction) {
+    throw new Error("Invalid transaction to approve")
+  }
+
+  // update data transactions
+  transaction.approved = value
+  data.transactions = data.transactions.map((currentTransaction) =>
+    currentTransaction.id === transactionId ? transaction : currentTransaction
+  )
+
+  return transaction
+}
